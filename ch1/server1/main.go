04ddbfb664770a42f0ca67ee70ac5823273bca32
noprom <tyee.noprom@qq.com>
2017-08-20 09:05:13 +0800

    bug fix

--- ch1/server1/main.go
+++ ch1/server1/main.go
@@ -11,6 +11,6 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "URL.Path = %q\n", r.URL.Path)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
